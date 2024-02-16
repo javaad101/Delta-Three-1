f0e785fc465c7a0205a868c20d8cba6b95f1f699
Javaad Ali <javaad.ail@advocatesclose.nyc>
2024-02-16 01:15:04 -0500

    Initial Commit

---
+++ Delta-Three/Delta_ThreeApp.swift
@@ -0,0 +1,32 @@
+//
+//  Delta_ThreeApp.swift
+//  Delta-Three
+//
+//  Created by AdvocatesClose on 2/16/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Delta_ThreeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Delta-Three/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Delta-Three
+//
+//  Created by AdvocatesClose on 2/16/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
